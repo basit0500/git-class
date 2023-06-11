4640870578d5240044099bff95e20842232ad461
basit0500 <basitkhkh1212@gmail.com>
2023-06-11 05:30:00 -0700

    first commit

---
+++ main.dart
@@ -0,0 +1,18 @@
+void main() {
+String email = "basitkhkh1212@gmail.com";
+String password = "basitkhkh";
+if (email == "basitkhkh1212@gmail.com" && password == "basitkhkh"){
+ print("succesful");
+  } else {
+    if(email != "basitkhkh1212@gmail.com"  && password != "basitkhkh"){
+      print("email password was inncorrect");
+    }
+    else if (email == "basitkhkh1212@gmail.com"){
+      print("email is inncorrect");
+    } else {
+      print("password is incorrect");
+    }
+  }
+}
+
+
